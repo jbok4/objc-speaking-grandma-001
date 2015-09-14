e5e808ed53c444a61f40ec29243c3b9e4ee59ba2
jbok4 <jbok4@aol.com>
2015-09-14 10:14:12 -0400

    Grandmas says its ok

--- objc-speaking-grandma/FISAppDelegate.m
+++ objc-speaking-grandma/FISAppDelegate.m
@@ -16,6 +16,26 @@
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    NSString *talkToGrandma = @"HI, GRANDMA!";
+    NSString *shoutAtGrandma = [talkToGrandma uppercaseString];
+    BOOL shouting = [talkToGrandma isEqualToString:shoutAtGrandma];
+    
+    talkToGrandma = @"Hi, Grandma!";
+    shoutAtGrandma = [talkToGrandma uppercaseString];
+
+    
+    if (shouting) {
+        NSLog(@"NO, NOT SINCE 1938!");
+    }
+    if (![talkToGrandma isEqualToString:shoutAtGrandma]) {
+        NSLog(@"WHAT'S THAT? SPEAK UP, DEAR!");
+    } else {
+        NSLog(@"NO, NOT SINCE 1938!");
+        
+        
+    }
+    
     // Override point for customization after application launch.
     
     /**
